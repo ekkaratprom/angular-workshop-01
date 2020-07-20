1f3af607888e8d474568164ff85e7582d468d5e8
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-07-20 15:14:35 +0700

    + saveName()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'Ekkarat';
+  score = 100;
+  show = true ;
+  firstName = '';
+  lastName = '';
+  name = '';
+  colors = ['red','green','blue'] ; //array
+
+  inputYourName(event: any){
+    this.firstName = event.target.value;
+  }
+
+  inputLastname(input: string): void{
+    this.lastName = input;
+  }
+
+  saveName() : void {
+      this.name = this.firstName +' '+ this.lastName ; // String concat
+      /* this.name = `$(firstName) $(lastName)`; */ // String template 
+  
+  }
+
+  
 }
